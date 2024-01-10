d500809317603c31fbd6eae37dda1d276bdf0c40
speedphp <jake@speedphp.com>
2024-01-10 14:45:18 +0800

    3.1.4

--- 2.3.3/test/test-merge-config.ts
+++
@@ -1,5 +0,0 @@
-const config = require("./config.json");
-const testConfig = require("./config-test.json");
-
-let realConfig = Object.assign(config, testConfig);
-console.log(realConfig);
---
+++ 3.1.4/test-express.ts
@@ -0,0 +1,13 @@
+// TODO: 3.1.4
+// import * as express from "express";
+
+// const port: number = 3000;
+// const app: express.Application = express();
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//     console.log("server start at port: " + port);
+// });
