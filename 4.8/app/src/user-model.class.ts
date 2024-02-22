5cf9b707d837217175e256188cf843969915bc11
speedphp <jake@speedphp.com>
2024-02-22 18:04:11 +0800

    4.8

---
+++ 4.8/app/src/user-model.class.ts
@@ -0,0 +1,50 @@
+import { log, Model } from "../../src/typespeed";
+import UserDto from "./entities/user-dto.class";
+
+export default class UserModel extends Model {
+
+
+    async getUsers() {
+        const users = await this.findAll({
+            id: { $lt: 10, $lte: 20 }, "name": { $like: "%a%" },
+            $or: [{ id: 1 }, { id: 2 }]
+        }, "id asc", "*", { page: 1, pageSize: 10 });
+        log("users", users);
+        return "getUsers";
+    }
+
+    async getUser(id: number) {
+        const user = await this.find({ id: id }, "id asc", "*");
+        log("user", user);
+        return "getUser";
+    }
+
+    async newUsers() {
+        const newId = this.random(1000, 2000);
+        await this.create([
+            new UserDto(newId, "UserDto " + newId),
+            new UserDto(newId+1, "UserDto " + (newId+1)),
+            { id: newId+2, name: "UserDto " + (newId+2) },
+        ]);
+        return newId;
+    }
+
+    async remove(id: number) {
+        const result = await this.delete({ id: id });
+        return "remove rows: " + result;
+    }
+
+    async count():Promise<number> {
+        const result = await this.findCount("1");
+        return result;
+    }
+
+    async editUser(id: number, name: string) {
+        const effectRows = await this.update({ id: id }, { name: name });
+        return effectRows;
+    }
+
+    private random(min, max) {
+        return Math.round(Math.random() * (max - min)) + min;
+    }
+}
---
+++ 4.8/src/default/socket-io.class.ts
@@ -0,0 +1,68 @@
+import { Server as IoServer } from "socket.io";
+import { createServer } from "http";
+
+let io: IoServer = null;
+const listeners = { "event": [], "disconnect": null, "error": null, "connected": null };
+
+class SocketIo {
+
+    public static setIoServer(app, ioSocketConfig) {
+        const httpServer = createServer(app);
+        io = new IoServer(httpServer, ioSocketConfig);
+        io.use((socket, next) => {
+            if (listeners["connected"] !== null) {
+                listeners["connected"](socket, async (err) => {
+                    if (listeners["error"] !== null && err) {
+                        await listeners["error"](socket, err);
+                    }
+                });
+            }
+            next();
+        });
+        io.on("connection", (socket) => {
+            if (listeners["disconnect"] !== null) {
+                socket.on("disconnect", async (reason) => {
+                    await listeners["disconnect"](socket, reason);
+                });
+            }
+            socket.use(async ([event, ...args], next) => {
+                try {
+                    for (let listener of listeners["event"]) {
+                        if (listener[1] === event) {
+                            await listener[0](socket, ...args);
+                        }
+                    }
+                } catch (err) {
+                    next(err);
+                }
+            });
+
+            if (listeners["error"] !== null) {
+                socket.on("error", async (err) => {
+                    await listeners["error"](socket, err);
+                });
+            }
+        });
+        return httpServer;
+    }
+
+    public static onEvent(event: string) {
+        return (target: any, propertyKey: string) => {
+            listeners["event"].push([target[propertyKey], event]);
+        }
+    }
+
+    public static onError(target: any, propertyKey: string) {
+        listeners["error"] = target[propertyKey];
+    }
+
+    public static onDisconnect(target: any, propertyKey: string) {
+        listeners["disconnect"] = target[propertyKey];
+    }
+
+    public static onConnected(target: any, propertyKey: string) {
+        listeners["connected"] = target[propertyKey];
+    }
+}
+
+export { SocketIo, io }
