3db68d90791987510afe3470151542f16a358e6a
speedphp <jake@speedphp.com>
2024-02-22 16:14:05 +0800

    4.5

---
+++ 4.5/src/database/query-decorator.ts
@@ -0,0 +1,201 @@
+import { createPool, ResultSetHeader } from 'mysql2';
+import { config, log } from '../speed';
+import BeanFactory from "../bean-factory.class";
+//import CacheFactory from '../factory/cache-factory.class';
+const pool = createPool(config("mysql")).promise();
+const paramMetadataKey = Symbol('param');
+const resultTypeMap = new Map<string, object>();
+const cacheDefindMap = new Map<string, number>();
+const tableVersionMap =  new Map<string, number>();
+//let cacheBean: CacheFactory;
+
+function Insert(sql: string) {
+    return (target, propertyKey: string, descriptor: PropertyDescriptor) => {
+        descriptor.value = async (...args: any[]) => {
+            const result: ResultSetHeader = await queryForExecute(sql, args, target, propertyKey);
+            // TODO
+            // if(cacheBean && result.affectedRows > 0){
+            //     const [tableName, tableVersion] = getTableAndVersion("insert", sql);
+            //     tableVersionMap.set(tableName, tableVersion + 1);
+            // }
+            return result.insertId;
+        };
+    };
+}
+
+function Update(sql: string) {
+    return (target, propertyKey: string, descriptor: PropertyDescriptor) => {
+        descriptor.value = async (...args: any[]) => {
+            const result: ResultSetHeader = await queryForExecute(sql, args, target, propertyKey);
+            // TODO
+            // if(cacheBean && result.affectedRows > 0){
+            //     const [tableName, tableVersion] = getTableAndVersion("update", sql);
+            //     tableVersionMap.set(tableName, tableVersion + 1);
+            // }
+            return result.affectedRows;
+        };
+    };
+}
+
+function Delete(sql: string) {
+    return (target, propertyKey: string, descriptor: PropertyDescriptor) => {
+        descriptor.value = async (...args: any[]) => {
+            const result: ResultSetHeader = await queryForExecute(sql, args, target, propertyKey);
+            // TODO
+            // if(cacheBean && result.affectedRows > 0){
+            //     const [tableName, tableVersion] = getTableAndVersion("delete", sql);
+            //     tableVersionMap.set(tableName, tableVersion + 1);
+            // }
+            return result.affectedRows;
+        };
+    };
+}
+// 查询装饰器
+function Select(sql: string) {
+    return (target, propertyKey: string, descriptor: PropertyDescriptor) => {
+        descriptor.value = async (...args: any[]) => {
+            let newSql = sql;
+            let sqlValues = [];
+            if (args.length > 0) {
+                // 处理参数绑定
+                [newSql, sqlValues] = convertSQLParams(args, target, propertyKey, newSql);
+            }
+            let rows;
+            // 检查当前查询是否需要缓存
+            // TODO
+            // if (cacheBean && cacheDefindMap.has([target.constructor.name, propertyKey].toString())) {
+            //     // 获取表名和表版本号
+            //     const [tableName, tableVersion] = getTableAndVersion("select", newSql);
+            //     // 构建缓存键
+            //     const cacheKey = JSON.stringify([tableName, tableVersion, newSql, sqlValues]);
+            //     if (cacheBean.get(cacheKey)) {
+            //         // 如果有结果则返回
+            //         rows = cacheBean.get(cacheKey);
+            //     } else {
+            //         // 查询结果
+            //         [rows] = await pool.query(newSql, sqlValues);
+            //         log("cache miss, and select result for " + rows);
+            //         const ttl = cacheDefindMap.get([target.constructor.name, propertyKey].toString());
+            //         // 存入缓存
+            //         cacheBean.set(cacheKey, rows, ttl);
+            //     }
+            // } else {
+            //     [rows] = await pool.query(newSql, sqlValues);
+            // }
+
+            if (rows === null || Object.keys(rows).length === 0) {
+                return;
+            }
+            const records = [];
+            // 取得@ResultType装饰器记录的数据类型
+            const resultType = resultTypeMap.get([target.constructor.name, propertyKey].toString());
+            if(!resultType){
+                return rows;
+            }
+            // 遍历查询结果记录，每行记录都创建一个数据类来装载
+            for (const rowIndex in rows) {
+                const entity = Object.create(resultType);
+                Object.getOwnPropertyNames(resultType).forEach(function (propertyRow) {
+                    // 匹配数据类的属性和字段名，对应赋值
+                    if (rows[rowIndex].hasOwnProperty(propertyRow)) {
+                        Object.defineProperty(
+                            entity,
+                            propertyRow,
+                            Object.getOwnPropertyDescriptor(rows[rowIndex], propertyRow),
+                        );
+                    }
+                });
+                // 组成数据类数组
+                records.push(entity);
+            }
+            return records;
+        };
+    }
+}
+
+function ResultType(dataClass) {
+    return function (target, propertyKey: string) {
+        resultTypeMap.set([target.constructor.name, propertyKey].toString(), new dataClass());
+        //never return
+    };
+}
+
+function Param(name: string) {
+    return function (target: any, propertyKey: string | symbol, parameterIndex: number) {
+        const existingParameters: [string, number][] = Reflect.getOwnMetadata(paramMetadataKey, target, propertyKey) || [];
+        existingParameters.push([name, parameterIndex]);
+        Reflect.defineMetadata(paramMetadataKey, existingParameters, target, propertyKey);
+    };
+}
+
+async function queryForExecute(sql: string, args: any[], target, propertyKey: string,): Promise<ResultSetHeader> {
+    let sqlValues = [];
+    let newSql = sql;
+    if (args.length > 0) {
+        [newSql, sqlValues] = convertSQLParams(args, target, propertyKey, newSql);
+    }
+    const [result] = await pool.query(newSql, sqlValues);
+    return <ResultSetHeader>result;
+}
+
+function convertSQLParams(args: any[], target: any, propertyKey: string, decoratorSQL: string,): [string, any[]] {
+    const queryValues = [];
+    let argsVal;
+    if (typeof args[0] === 'object') {
+        argsVal = new Map(
+            Object.getOwnPropertyNames(args[0]).map((valName) => [valName, args[0][valName]]),
+        );
+    } else {
+        const existingParameters: [string, number][] = Reflect.getOwnMetadata(paramMetadataKey, target, propertyKey);
+        argsVal = new Map(
+            existingParameters.map(([argName, argIdx]) => [argName, args[argIdx]]),
+        );
+    }
+    const regExp = /#{(\w+)}/;
+    let match;
+    while (match = regExp.exec(decoratorSQL)) {
+        const [replaceTag, matchName] = match;
+        decoratorSQL = decoratorSQL.replace(new RegExp(replaceTag, 'g'), '?');
+        queryValues.push(argsVal.get(matchName));
+    }
+    return [decoratorSQL, queryValues];
+}
+
+// TODO
+// function cache(ttl: number) {
+//     return function (target: any, propertyKey: string) {
+//         // 收集需要缓存的查询方法和缓存过期时间
+//         cacheDefindMap.set([target.constructor.name, propertyKey].toString(), ttl);
+//         if (cacheBean == null) {
+//             const cacheFactory = BeanFactory.getBean(CacheFactory);
+//             if (cacheFactory || cacheFactory["factory"]) {
+//                 cacheBean = cacheFactory["factory"];
+//             }
+//         }
+//         log(cacheDefindMap);
+//     }
+// }
+
+// function getTableAndVersion(name: string, sql: string): [string, number] {
+//     const regExpMap = {
+//         insert: /insert\sinto\s+([\w`\'\"]+)/i,
+//         update: /update\s+([\w`\'\"]+)/i,
+//         delete: /delete\sfrom\s+([\w`\'\"]+)/i,
+//         select: /\s+from\s+([\w`\'\"]+)/i
+//     }
+//     const macths = sql.match(regExpMap[name]);
+//     if (macths && macths.length > 1) {
+//         const tableName = macths[1].replace(/[`\'\"]/g, "");
+//         const tableVersion = tableVersionMap.get(tableName) || 1;
+//         tableVersionMap.set(tableName, tableVersion);
+//         log(tableVersionMap);
+//         return [tableName, tableVersion];
+//     }else{
+//         throw new Error("can not find table name");
+//     }
+// }
+
+export { Insert, Update, Delete, Select, Param, ResultType
+    //, getTableAndVersion
+    //, cache 
+};
---
+++ 4.5/test/test-database.class.ts
@@ -0,0 +1,99 @@
+//import CacheFactory from "../src/factory/cache-factory.class";
+import { Insert, Update, Select, Param, ResultType } from "../src/database/query-decorator";
+//import { cache, getTableAndVersion } from "../src/database/query-decorator";
+import { GetMapping } from "../src/route-mapping.decorator";
+import { onClass, log, autoware } from "../src/speed";
+import UserDto from "./entities/user-dto.class";
+
+@onClass
+export default class TestDatabase {
+
+    // TODO
+    // @autoware
+    // private cacheBean: CacheFactory;
+
+    @GetMapping("/db/insert")
+    async insert(req, res) {
+        const id = req.query.id || 1;
+        const newId = await this.addRow("new name " + id, id);
+        log("Insert newId: " + newId);
+        res.send("Insert success");
+    }
+
+    @GetMapping("/db/insert2")
+    async insertByObject(req, res) {
+        const newId = await this.addRowByObject({
+            "id": 25, "name": "new name 25"
+        });
+        log("Insert newId: " + newId);
+        res.send("Insert success");
+    }
+
+    @GetMapping("/db/update")
+    async update(req, res) {
+        const affectedRows = await this.editRow();
+        log("Update rows: " + affectedRows);
+        res.send("update success");
+    }
+
+    @GetMapping("/db/select")
+    async select(req, res) {
+        const rows = await this.selectRow();
+        log("select rows: " + rows);
+        res.send(rows);
+    }
+
+    @GetMapping("/db/select1")
+    async selectById(req, res) {
+        const row = await this.findRow(req.query.id || 1);
+        log("select rows: " + row);
+        res.send(row);
+    }
+
+    @GetMapping("/db/select-user")
+    async selectUser(req, res) {
+        const users: UserDto[] = await this.findUsers();
+        log("select users: " + users);
+        res.send(users);
+    }
+
+    // TODO
+    // @GetMapping("/db/set-cache")
+    // testCache(req, res) {
+    //     this.cacheBean.set("test", req.query.value || "test");
+    //     res.send("set cache success");
+    // }
+
+    // @GetMapping("/db/get-cache")
+    // displayCache(req, res) {
+    //     res.send(this.cacheBean.get("test"));
+    // }
+
+    // @GetMapping("/db/check-sql")
+    // async checkSql(req, res) {
+    //     const sql = 'Insert into `user` (id, name) values (#{id}, #{name})';
+    //     const [tableName, tableVersion] = getTableAndVersion(sql);
+    //     res.send(tableName);
+    // }
+
+    @Insert("Insert into `user` (id, name) values (#{id}, #{name})")
+    private async addRow(@Param("name") newName: string, @Param("id") id: number) { }
+
+    @Insert("Insert into `user` (id, name) values (#{id}, #{name})")
+    private async addRowByObject(myParams: object) { }
+
+    @Update("Update `user` set `name` = 'test5' where id = 5")
+    private async editRow() { }
+
+    @Select("Select * from `user`")
+    private async selectRow() { }
+
+    // TODO
+    //@cache(1800)
+    @Select("Select * from `user` where id = #{id}")
+    private async findRow(@Param("id") id: number) { }
+
+    @ResultType(UserDto)
+    @Select("Select * from `user`")
+    private findUsers(): UserDto[] { return; }
+}
