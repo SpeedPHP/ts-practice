1b2f97f01c66329e0461cfb113294371c0ec47d1
speedphp <jake@speedphp.com>
2024-02-23 16:32:26 +0800

    5.5

---
+++ 5.5/app/main.ts
@@ -0,0 +1,14 @@
+import { app, autoware, ServerFactory } from "typespeed";
+import { swaggerMiddleware } from "../index";
+
+@app
+class Main {
+
+    @autoware
+    public server : ServerFactory;
+
+    public main(){
+        swaggerMiddleware(this.server.app);
+        this.server.start(8081);
+    }
+}
