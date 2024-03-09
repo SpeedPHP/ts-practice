483994bf49c250eda9f11c34195288d1338aceef
speedphp <jake@speedphp.com>
2024-03-09 16:03:43 +0800

    4

--- 1.4.4/db-init.ts
+++ 1.4.4/db-init.ts
@@ -1,53 +1,59 @@
-// TODO
-// import {createServer} from "http";
-// import { createConnection } from "mysql2";
+import {createServer} from "http";
+import {createConnection} from 'mysql2';
 
-// interface Page {
-//   display(req, res):void;
-// }
+// 页面接口类
+interface Page{
+  display(req, res);
+}
 
-// class User implements Page{
-//   display(req: any, res: any): void {
-//     const conn = createConnection({
-//       host: "localhost", 
-//       user: "root",
-//       password : "root",
-//       database: "test"
-//     });
-//     conn.query("select * from `user`", (err, results) => {
-//       console.log(err);
-//       res.end(JSON.stringify(results));
-//     })
-//   }
-// }
+// 查询用户页面
+class User implements Page {
+  display(req, res) {
+    const conn = createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "test"
+    });
+    conn.query("select * from `user`", (err, results) => {
+      console.log(err);
+      res.end(JSON.stringify(results));
+    })
+  }
+}
 
-// class Second implements Page{
-//   display(req: any, res: any): void {
-//     res.end("I am the Second Page");
-//   }
-// }
+// 页面类
+class First implements Page{
+  display(req, res) {
+    res.end("I am the First Page");
+  }
+}
 
-// class First implements Page {
-//   display(req: any, res: any): void {
-//     res.end("I am the First Page")
-//   }
-// }
+// 第二个页面类
+class Second implements Page{
+  display(req, res) {
+    res.end("I am the Second Page");
+  }
+}
 
-// class Root implements Page{
-//   display(req: any, res: any): void {
-//     res.end("I am the Main page")
-//   }
-// }
+// 默认页面类
+class Root implements Page{
+  display(req, res) {
+    res.end("I am the Main Page");
+  }
+}
 
-// const router = new Map<string, Page>();
-// router.set("/first", new First());
-// router.set("/second", new Second());
-// router.set("/user", new User());
-// router.set("/main", new Root());
-// createServer((req, res) => {
-//   let page = router.get(req.url === undefined ? "" : req.url);
-//   if(page === undefined) {
-//     page = new Root();
-//   }
-//   page.display(req, res);
-// }).listen(3000);
+// 路由变量
+const router = new Map<string, Page>();
+router.set("/first", new First);
+router.set("/second", new Second);
+router.set("/user", new User);
+router.set("/main", new Root);
+
+createServer((req, res) => {
+  let page = router.get(req.url == undefined ? "" : req.url);
+  if(page == undefined) {
+    page = new Root();
+  }
+  page.display(req, res);
+}).listen(3000);
--- 1.4.4/db-singleton.ts
+++ 1.4.4/db-singleton.ts
@@ -1,76 +1,87 @@
-// TODO
-// import {createServer} from "http";
-// import { createConnection } from "mysql2";
+import {createServer} from "http";
+import {createConnection} from 'mysql2';
 
-// interface Page {
-//   display(req, res):void;
-// }
+// 页面接口类
+interface Page{
+  display(req, res);
+}
 
-// class User implements Page{
-//   display(req: any, res: any): void {
-//     const db = Database.getInstance();
-//     const db2 = Database.getInstance();
-//     console.log(db === db2);
+// 查询用户页面
+class User implements Page {
+  display(req, res) {
+    const conn = Database.getInstance();
+    const conn2 = Database.getInstance();
 
-//     db.query("select * from `user`", (err, results) => {
-//       res.end(JSON.stringify(results));
-//     })
-//   }
-// }
+    console.log(conn === conn2);
 
-// class Second implements Page{
-//   display(req: any, res: any): void {
-//     res.end("I am the Second Page");
-//   }
-// }
+    conn.query("select * from `user` where id in (28, 29)", (err, results) => {
+      console.log(err);
+      res.end(JSON.stringify(results));
+    })
+  }
+}
 
-// class First implements Page {
-//   display(req: any, res: any): void {
-//     res.end("I am the First Page")
-//   }
-// }
+// 数据库链接类
+class Database {
+  private static instance; // 实例
+  private connection;
+  private constructor(connection) {
+    this.connection = connection;
+  }
 
-// class Root implements Page{
-//   display(req: any, res: any): void {
-//     res.end("I am the Main page")
-//   }
-// }
+  // 获取实例的静态方法 -- 单例方法
+  static getInstance() {
+    if(!Database.instance){
+      Database.instance = new Database(
+        createConnection({
+          host: "localhost",
+          user: "root",
+          password: "root",
+          database: "test"
+        })
+      );
+    }
+    return Database.instance;
+  }
 
-// class Database {
-//   private static instance: Database;
-//   private connection;
-//   private constructor(connection) {
-//     this.connection = connection;
-//   }
+  query(sql, callback) {
+    this.connection.query(sql, callback);
+  }
+}
 
-//   static getInstance() {
-//     if(!Database.instance) {
-//       Database.instance = new Database(
-//         createConnection({
-//           host: "localhost", 
-//           user: "root",
-//           password : "root",
-//           database: "test"
-//         })
-//       );
-//     }
-//     return Database.instance;
-//   }
+// 页面类
+class First implements Page{
+  display(req, res) {
+    res.end("I am the First Page");
+  }
+}
 
-//   query(sql, callback) {
-//     this.connection.query(sql, callback);
-//   }
-// }
+// 第二个页面类
+class Second implements Page{
+  display(req, res) {
+    const conn = Database.getInstance();
+    res.end("I am the Second Page");
+  }
+}
 
-// const router = new Map<string, Page>();
-// router.set("/first", new First());
-// router.set("/second", new Second());
-// router.set("/user", new User());
-// router.set("/main", new Root());
-// createServer((req, res) => {
-//   let page = router.get(req.url === undefined ? "" : req.url);
-//   if(page === undefined) {
-//     page = new Root();
-//   }
-//   page.display(req, res);
-// }).listen(3000);
+// 默认页面类
+class Root implements Page{
+  display(req, res) {
+    res.end("I am the Main Page");
+  }
+}
+
+// 路由变量
+const router = new Map<string, Page>();
+router.set("/first", new First);
+router.set("/second", new Second);
+router.set("/user", new User);
+router.set("/main", new Root);
+
+createServer((req, res) => {
+  let page = router.get(req.url == undefined ? "" : req.url);
+  if(page == undefined) {
+    page = new Root();
+  }
+  page.display(req, res);
+}).listen(3000);
