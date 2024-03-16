0489d3274972cfc4f13c2dcf82b74337e6133583
speedphp <jake@speedphp.com>
2024-03-16 19:49:17 +0800

    30

--- 5.2.3/app/src/test-io.class.ts
+++ 5.2.3/app/src/test-io.class.ts
@@ -1,24 +1,19 @@
-import { resource, getMapping,value, component } from "../../src/typespeed";
-//import { SocketIo } from "../../src/typespeed";
+import { resource, getMapping, log, component, SocketIo } from "../../src/typespeed";
 @component
 export default class TestIo {
-    // TODO
-    // 注入Socket.IO对象
-    // @resource()
-    // public socketIo: SocketIo;
 
-    // // 测试接收事件
-    // @SocketIo.onEvent("test")
-    // public connection(socket, message) {
-    //     console.log(message);
-    //     // 使用两种方式发送广播
-    //     this.socketIo.sockets.emit("test","test-from-server2");
-    //     socket.emit("test", "test-from-server3");
-    // }
+    @resource()
+    socketIo: SocketIo;
 
-    // // 显示页面
-    // @getMapping("/socketIo")
-    // public socketIoPage(req, res) {
-    //     res.render("socket");
-    // }
+    @SocketIo.onEvent("test")
+    connection(socket, message) {
+        log(message)
+        this.socketIo.sockets.emit("test", "test-from-server-2");
+        socket.emit("test", "test-from-server-3");
+    }
+
+    @getMapping("/socket.html")
+    socketPage(req, res) {
+        res.render("socket");
+    }
 }
--- 5.2.3/src/default/socket-io.class.ts
+++ 5.2.3/src/default/socket-io.class.ts
@@ -1,40 +1,31 @@
-// import { Server as IoServer } from "socket.io";
-// TODO
-// // 创建Socket.IO对象
-// const ioObj:IoServer = new IoServer({
-//     cors: {
-//       origin: "http://localhost:8080"
-//     }
-//   });
+import {log} from "../core.decorator";
+import {Server as IoServer} from "socket.io";
 
-// class SocketIo extends IoServer {
+const ioObj: IoServer = new IoServer ({
+  cors: {
+    origin: "http://localhost:8080"
+  }
+});
 
-//     constructor() {
-//         super();
-//         return ioObj;
-//     }
+class SocketIo extends IoServer {
+  constructor() {
+    super();
+    return ioObj;
+  }
 
-//     // 监听事件的装饰器
-//     public static onEvent(event: string) {
-//         console.log("onEvent");
-//         return (target: any, propertyKey: string) => {
-//             console.log("onEvent-callback");
-//             ioObj.on("connection", (socket) => {
-//                 console.log("onEvent-connection")
-//                 // 监听事件
-//                 socket.on(event, (message) => {
-//                     console.log("onEvent-socket",event, message)
-//                     ioObj.emit("test", "test-from-server1");
-//                     target[propertyKey](socket, message);
-//                 });
-//             });
-//             // 启动Socket.IO服务
-//             ioObj.listen(8085);
-//         }
-//     }
+  public static onEvent(event) {
+    return (target, propertyKey) => {
+      ioObj.on("connection", (socket) => {
+        log("Connected");
+        socket.on(event, (message) => {
+          log("On Event", event);
+          ioObj.emit("test", "test-from-server-1");
+          target[propertyKey](socket, message);
+        })
+      })
+      ioObj.listen(8085);
+    }
+  }
+}
 
-
-
-// }
-
-// export { SocketIo }
+export {SocketIo}
