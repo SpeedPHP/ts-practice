bf030ff7a81d097414e0e34bff908e27fafc9199
speedphp <jake@speedphp.com>
2024-03-17 15:39:48 +0800

    37

--- 6.1.2/test.ts
+++ 6.1.2/test.ts
@@ -4,8 +4,14 @@ function sayHello(name: string) {
     return "Hello " + name + "!";
 }
 
+// 测试集
 describe("Test Demo!", () => {
-    it("should return 'Hello World!'", () => {
+    before(() => {
+        
+    })
+
+    // 单个测试
+    it("Should return 'Hello World!'", () => {
         expect(sayHello("World")).to.equal("Hello World!");
     });
 });
