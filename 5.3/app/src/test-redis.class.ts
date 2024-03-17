59fd49f150352cc2975422a68e948d312ef4cf2a
speedphp <jake@speedphp.com>
2024-03-17 08:23:35 +0800

    32

--- 5.3/app/src/test-redis.class.ts
+++ 5.3/app/src/test-redis.class.ts
@@ -1,46 +1,45 @@
-// TODO
-// import { Redis, component, redisSubscriber, autoware, getMapping, log, reqQuery } from "../../src/typespeed";
+import { Redis, component, autoware, getMapping, log, reqQuery, redisSubscriber } from "../../src/typespeed";
 
-// @component
-// export default class TestRedis {
-//     @autoware
-//     private redisObj: Redis;
+@component
+export default class TestRedis {
 
-//     @redisSubscriber("mychannel")
-//     public listen(message) {
-//         log("Received by Decorator '%s'", message);
-//     }
+  @autoware
+  private redisObj: Redis;
 
-//     @getMapping("/redis/publish")
-//     async redisPublish() {
-//         await this.redisObj.publish("mychannel", "Hello World");
-//         return "Published!";
-//     }
+  @getMapping("/redis")
+  async redisString() {
+      await this.redisObj.set("redisKey", "Hello World");
+      const value = await this.redisObj.get("redisKey");
+      return "get from redis: " + value;
+  }
 
-//     @getMapping("/redis")
-//     async redisString() {
-//         await this.redisObj.set("redisKey", "Hello World");
-//         const value = await this.redisObj.get("redisKey");
-//         return "get from redis: " + value;
-//     }
+  @redisSubscriber("mychannel")
+  listen(message) {
+    log("Received by Decorator: ", message);
+  }
 
-//     @getMapping("/redis/add")
-//     async addZset(@reqQuery name: string, @reqQuery score: number) {
-//         log("add zset: %s, %s", name, score)
-//         await this.redisObj.zadd("scoreSet", score, name);
-//         return "add zset success";
-//     }
+  @getMapping("/publish")
+  async publish() {
+      await this.redisObj.publish("mychannel", "Hello World");
+      return "ok";
+  }
 
-//     @getMapping("/redis/ranking")
-//     async listRanking() {
-//         const list = await this.redisObj.zrevranking("scoreSet", 0, -1);
-//         return Object.fromEntries(list);
-//     }
 
-//     @getMapping("/redis/list")
-//     async listZset() {
-//         const list = await this.redisObj.zranking("scoreSet", 0, -1);
-//         log("list zset: %s", JSON.stringify(Object.fromEntries(list)));
-//         return Object.fromEntries(list);
-//     }
-// }
+  @getMapping("/zadd")
+  async zadd(@reqQuery name, @reqQuery score) {
+      await this.redisObj.zadd("MyScoreSet", score, name);
+      return "ok";
+  }
+
+  @getMapping("/read")
+  async readScore() {
+      const result = await this.redisObj.zrevranking("MyScoreSet", 0, -1)
+      return Object.fromEntries(result);
+  }
+
+  @getMapping("/readscore")
+  async readWithScore() {
+      const result = await this.redisObj.zrevrange("MyScoreSet", 0, -1, "WITHSCORES")
+      return JSON.stringify(result);
+  }
+}
--- 5.3/src/default/redis.class.ts
+++ 5.3/src/default/redis.class.ts
@@ -1,92 +1,84 @@
-// TODO
-// import { bean } from "../core.decorator";
-// import { Redis as IoRedis, RedisKey} from "ioredis";
-// import { config } from "../typespeed";
+import { bean } from "../core.decorator";
+import { Redis as IoRedis, RedisKey } from "ioredis";
+import {config} from "../typespeed";
+import { channel } from "diagnostics_channel";
+const redisSubscribers = {};
 
-// // 收集Redis订阅事件
-// const redisSubscribers = {};
+class Redis extends IoRedis {
 
-// class Redis extends IoRedis {
-//     // 分别创建发布对象和订阅对象
-//     private static pubObj: Redis = null;
-//     private static subObj: Redis = null;
+  private static pubObj: Redis = null;
+  private static subObj: Redis = null;
 
-//     // 提供Redis对象，这里是发布对象
-//     @bean
-//     public getRedis(): Redis {
-//         return Redis.getInstanceOfRedis("pub");
-//     }
+  @bean
+  public getRedis(): Redis {
+    return Redis.getIntanceOfRedis("pub");
+  }
 
-//     // 获取排行榜数据，按分数从高到低
-//     public async zrevranking(key: RedisKey, start: number | string, stop: number | string): Promise<Map<string, number>> {
-//         // 用zrevrange()获得数据
-//         const list = await this.zrevrange(key, start, stop, "WITHSCORES");
-//         // 转换数据格式，方便开发者直接使用
-//         const map = new Map<string, number>();
-//         for (let i = 0; i < list.length; i = i + 2) {
-//             map.set(list[i], Number(list[i + 1]));
-//         }
-//         return map;
-//     }
+  static getIntanceOfRedis(mode: "sub" | "pub") {
+    if( !config("redis") ){
+      return null;
+    }
+    if(mode == "pub") {
+      this.pubObj = this.pubObj || new Redis(config("redis"));
+      return this.pubObj;
+    } else {
+      this.subObj = this.subObj || new Redis(config("redis"));
+      return this.subObj;
+    }
+  }
 
-//     // 获取排行榜数据，按分数从低到高
-//     public async zranking(key: RedisKey, start: number | string, stop: number | string): Promise<Map<string, number>> {
-//         // zrange()获得数据
-//         const list = await this.zrange(key, start, stop, "WITHSCORES");
-//         // 转换数据格式，方便开发者直接使用
-//         const map = new Map<string, number>();
-//         for (let i = 0; i < list.length; i = i + 2) {
-//             map.set(list[i], Number(list[i + 1]));
-//         }
-//         return map;
-//     }
+  // 获取排行榜数据，按分数从高到低
+  public async zrevranking(key: RedisKey, start: number | string, stop: number | string): Promise<Map<string, number>> {
+      // 用zrevrange()获得数据
+      const list = await this.zrevrange(key, start, stop, "WITHSCORES");
+      // 转换数据格式，方便开发者直接使用
+      const map = new Map<string, number>();
+      for (let i = 0; i < list.length; i = i + 2) {
+          map.set(list[i], Number(list[i + 1]));
+      }
+      return map;
+  }
 
-//     // 获取实例化Redis对象，此处为单例模式
-//     static getInstanceOfRedis(mode: "sub" | "pub") {
-//         // 检查Redis配置，避免没有配置Redis时报错
-//         if (!config("redis")) {
-//             return null;
-//         }
-//         // 跟进参数分别创建发布对象和订阅对象
-//         if (mode === "sub") {
-//             this.pubObj = this.pubObj || new Redis(config("redis"));
-//             return this.pubObj;
-//         } else {
-//             this.subObj = this.subObj || new Redis(config("redis"));
-//             return this.subObj;
-//         }
-//     }
-// }
+    // 获取排行榜数据，按分数从低到高
+  public async zranking(key: RedisKey, start: number | string, stop: number | string): Promise<Map<string, number>> {
+        // zrange()获得数据
+        const list = await this.zrange(key, start, stop, "WITHSCORES");
+        // 转换数据格式，方便开发者直接使用
+        const map = new Map<string, number>();
+        for (let i = 0; i < list.length; i = i + 2) {
+            map.set(list[i], Number(list[i + 1]));
+        }
+        return map;
+    }
+}
 
-// // 订阅装饰器
-// function redisSubscriber(channel: string) {
-//     // 检查Redis配置，避免没有配置Redis时报错
-//     if (!config("redis")) return function(){
-//         throw new Error("redis not configured");
-//     };
-//     // 开启订阅
-//     Redis.getInstanceOfRedis("sub").subscribe(channel, function (err, count) {
-//         if (err) {
-//             console.error(err);
-//         }
-//     });
-//     // 收集订阅事件
-//     return function (target: any, propertyKey: string) {
-//         redisSubscribers[channel] = target[propertyKey];
-//     };
-// }
+if(config("redis")) {
+  process.once("SIGINT", () => {
+    Redis.getIntanceOfRedis("sub") || Redis.getIntanceOfRedis("sub").disconnect();
+    Redis.getIntanceOfRedis("pub") || Redis.getIntanceOfRedis("pub").disconnect();
+  })
+}
 
-// // 检查Redis配置，避免没有配置Redis时报错
-// if (config("redis")) {
-//     Redis.getInstanceOfRedis("sub").on("message", function (channel, message) {
-//         redisSubscribers[channel](message);
-//     });
-// }
+function redisSubscriber(channel) {
+  if(!config("redis")) {
+    return () => {
+      throw new Error("redis not configured");
+    }
+  }
+  Redis.getIntanceOfRedis("sub").subscribe(channel, (err, count) => {
+    if(err) {
+      console.error(err);
+    }
+  });
+  return (target, propertyKey) => {
+    redisSubscribers[channel] = target[propertyKey];
+  }
+}
 
-// // 进程退出时关闭Redis连接
-// process.once('SIGINT', () => { 
-//     Redis.getInstanceOfRedis("sub") || Redis.getInstanceOfRedis("sub").disconnect();
-//     Redis.getInstanceOfRedis("pub") || Redis.getInstanceOfRedis("pub").disconnect();
-// });
+if(config("redis")) {
+  Redis.getIntanceOfRedis("sub").on("message", (channel, message) => {
+    redisSubscribers[channel](message);
+  });
+}
 
-// export { Redis, redisSubscriber };
+export { Redis, redisSubscriber }
